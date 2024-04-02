f3025ef51782454d7c6d19b0d243c3202c93218d
Ghabriel <ghabrielmolina@hotmail.com>
2024-04-01 22:06:15 -0300

    feat: input styling with placeholder in participant list with lucide

--- src/_components/attendee-list.tsx
+++ src/_components/attendee-list.tsx
@@ -1,5 +1,14 @@
+import { Search } from 'lucide-react'
+
 export function AttendeeList() {
   return (
-    <h1>Participantes</h1>
+    <div className="flex gap-3 items-center">
+      <h1 className="text-2xl font-bold">Participantes</h1>
+
+      <div className="px-3 py-1.5 w-72 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+        <Search className='size-4 text-emerald-300' />
+        <input className="bg-transparent flex-1 outline-none" placeholder="Buscar Participante..." />
+      </div>
+    </div>
   )
 }
