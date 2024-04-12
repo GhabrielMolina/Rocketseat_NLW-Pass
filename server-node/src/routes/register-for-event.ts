43d5552f8cd880b7e615eb1bf42804af419a4f24
Ghabriel <ghabrielmolina@hotmail.com>
2024-04-12 12:09:07 -0300

    feat: rules for available places and a single email event for registering new participants

--- server-node/src/routes/register-for-event.ts
+++ server-node/src/routes/register-for-event.ts
@@ -26,8 +26,40 @@ export async function registerForEvent(app: FastifyInstance) {
       const { name, email } = request.body // Dentro do request.body, pegar o nome e o email do participante
       const { eventId } = request.params // Dentro do request.params, pegar o eventId
 
+      const attendeeFromEmail = await prisma.attendee.findUnique({ // Verificar se já existe um participante com o mesmo email
+        where: {
+          eventId_email: { // Composto de eventId e email (Chave composta) criada no schema.prisma
+            email,
+            eventId,
+          }
+        }
+      })
+      if (attendeeFromEmail != null) { // Caso o participante já esteja cadastrado no evento
+        throw new Error('Attendee already registered for this event.')
+      }
+
+      
+      const [event, amountOfAttendeesForEvent] = await Promise.all([ //  Para as duas querys do banco de dados executar em paralelo e não sequencialmente ja que leva algum tempo com o await
+
+        prisma.event.findUnique({ // Verificar se o evento existe e pega os dados dele
+          where: {
+            id: eventId,
+          }
+        }),
+
+        prisma.attendee.count({ // Verifica a quantidade de participantes no evento
+          where: {
+            eventId,
+          }
+        })
+      ])
+
+      if (event?.maximumAttendees && amountOfAttendeesForEvent >= event.maximumAttendees) { // Caso o evento tenha atingido o limite de participantes
+        throw new Error('Event is full.')
+      }
+
       // Cadastrando usuario no evento
-      const attendee = await prisma.attendee.create ({
+      const attendee = await prisma.attendee.create({
         data: {
           name,
           email,
@@ -35,6 +67,6 @@ export async function registerForEvent(app: FastifyInstance) {
         }
       })
 
-      return reply.status(201).send({attendeeId: attendee.id}) // Enviando o id do participante como resposta
+      return reply.status(201).send({ attendeeId: attendee.id }) // Enviando o id do participante como resposta
     })
 } // Registrar a rota de criação de participantes em um evento no server.ts
