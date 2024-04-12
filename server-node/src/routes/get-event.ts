5c2f9f8d94df3054bdd120d54d5f497f073744f0
Ghabriel <ghabrielmolina@hotmail.com>
2024-04-12 12:37:51 -0300

    featk: route to return important data from a given event to the server

---
+++ server-node/src/routes/get-event.ts
@@ -0,0 +1,65 @@
+// Rota para retornar todos os dados de um unico evento (Para poder aplicar em uma página de inscrição para evento)
+
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function getEvent(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get('/events/:eventId', {
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        response: {
+          200: {
+            event: z.object({
+              id: z.string().uuid(),
+              title: z.string(),
+              slug: z.string(),
+              details: z.string().nullable(),
+              maximumAttendees: z.number().int().nullable(),
+              attendeesAmount: z.number().int(),
+            })
+          }
+        },
+      }
+    }, async (request, reply) => {
+      const { eventId } = request.params
+
+      const event = await prisma.event.findUnique({ // Buscar o evento pelo ID
+        select: { // Selecionar os campos expecificos que serão retornados
+          id: true,
+          title: true,
+          slug: true,
+          details: true,
+          maximumAttendees: true,
+          _count: { // Contar o número de participantes cadastrados no evento
+            select: {
+              attendees: true,
+            }
+          },
+        },
+        where: {
+          id: eventId,
+        }
+      })
+
+      if (event == null) { // Se o evento não for encontrado, retornar um erro
+        throw new Error('Event not found')
+      }
+
+      return reply.send({
+        event: {
+          id: event.id,
+          title: event.title,
+          slug: event.slug,
+          details: event.details,
+          maximumAttendees: event.maximumAttendees,
+          attendeesAmount: event._count.attendees,
+        },
+      })
+    })
+} // Registrar a rota de dados dos eventos no server.ts
--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -2,6 +2,7 @@ import fastify from 'fastify';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'; // https://github.com/turkerdev/fastify-type-provider-zod
 import { createEvent } from './routes/create-event';
 import { registerForEvent } from './routes/register-for-event';
+import { getEvent } from './routes/get-event';
 
 const app = fastify();
 
@@ -11,6 +12,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent) // Registrar a rota de criação de eventos
 app.register(registerForEvent) // Registrar a rota de criação de participantes em um evento
+app.register(getEvent) // Registrar a rota de dados dos eventos
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server is running on port 3333');
