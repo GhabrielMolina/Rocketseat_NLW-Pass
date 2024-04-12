193f52133f97b0b040d3ef1768ebba41ba9fce1d
Ghabriel <ghabrielmolina@hotmail.com>
2024-04-12 13:05:50 -0300

    feat: route to return important data from a given participant to the server

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -3,6 +3,7 @@ import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod
 import { createEvent } from './routes/create-event';
 import { registerForEvent } from './routes/register-for-event';
 import { getEvent } from './routes/get-event';
+import { getAttendeeBadge } from './routes/get-attendee-badge';
 
 const app = fastify();
 
@@ -13,6 +14,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createEvent) // Registrar a rota de criação de eventos
 app.register(registerForEvent) // Registrar a rota de criação de participantes em um evento
 app.register(getEvent) // Registrar a rota de dados dos eventos
+app.register(getAttendeeBadge) // Registrar a rota de dados dos participantes
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server is running on port 3333');
