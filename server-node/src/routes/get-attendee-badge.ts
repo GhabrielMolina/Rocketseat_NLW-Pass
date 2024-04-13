5fb6d1713e9488ece585ddd85024c27d4480644c
Ghabriel <ghabrielmolina@hotmail.com>
2024-04-13 11:02:32 -0300

    feat: participant check-in route implementation

--- server-node/src/routes/get-attendee-badge.ts
+++ server-node/src/routes/get-attendee-badge.ts
@@ -13,7 +13,16 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         params: z.object({
           attendeeId: z.coerce.number().int(), // Coerção para número inteiro
         }),
-        response: {},
+        response: {
+          200: z.object({
+            badge: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              eventTitle: z.string(),
+              checkInURL: z.string().url(),
+            })
+          })
+        },
       }
     }, async (request, reply) => {
       const { attendeeId } = request.params
@@ -36,6 +45,17 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         throw new Error('Attendee not found')
       }
 
-      return reply.send({ attendee })
+      // Construir a URL de check-in do participante
+      const baseURL = `${request.protocol}://${request.hostname}`
+      const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+
+      return reply.send({ 
+        badge:{
+          name: attendee.name,
+          email: attendee.email,
+          eventTitle: attendee.event.title,
+          checkInURL: checkInURL.toString(),
+        } 
+      })
     })
 } // Registrar a rota de dados dos participantes no server.ts
