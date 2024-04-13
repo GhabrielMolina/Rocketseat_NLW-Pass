d675bd32fa0adf4bac8c4027c7a2e07245e5347f
Ghabriel <ghabrielmolina@hotmail.com>
2024-04-13 11:31:15 -0300

    feat: creation of the check-in route

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -4,6 +4,7 @@ import { createEvent } from './routes/create-event';
 import { registerForEvent } from './routes/register-for-event';
 import { getEvent } from './routes/get-event';
 import { getAttendeeBadge } from './routes/get-attendee-badge';
+import { checkIn } from './routes/check-in';
 
 const app = fastify();
 
@@ -15,6 +16,7 @@ app.register(createEvent) // Registrar a rota de criação de eventos
 app.register(registerForEvent) // Registrar a rota de criação de participantes em um evento
 app.register(getEvent) // Registrar a rota de dados dos eventos
 app.register(getAttendeeBadge) // Registrar a rota de dados dos participantes
+app.register(checkIn) // Registrar a rota de check-in
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server is running on port 3333');
