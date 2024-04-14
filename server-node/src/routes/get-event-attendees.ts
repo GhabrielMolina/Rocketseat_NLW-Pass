0783df38451a5db1dff13d8f378bbc5dcc74fe85
Ghabriel <ghabrielmolina@hotmail.com>
2024-04-14 14:21:22 -0300

    correction: counter in the API for the number of participants in an event

--- server-node/src/routes/get-event-attendees.ts
+++ server-node/src/routes/get-event-attendees.ts
@@ -21,46 +21,62 @@ export async function getEventAttendees(app: FastifyInstance) {
         }),
         response: {
           200: z.object({
-            attendees: z.array(z.object({
-              id: z.number(),
-              name: z.string(),
-              email: z.string().email(),
-              createdAt: z.date(),
-              checkedInAt: z.date().nullable(),
-            }))
-          })
+            attendees: z.array(
+              z.object({
+                id: z.number(),
+                name: z.string(),
+                email: z.string().email(),
+                createdAt: z.date(),
+                checkedInAt: z.date().nullable(),
+              })
+            ),
+            total: z.number(),
+          }),
         },
       }
     }, async (request, reply) => {
       const { eventId } = request.params
       const { query, pageIndex } = request.query // request.query é um objeto que contém as chaves e valores dos parâmetros de consulta da URL 
 
-      const attendees = await prisma.attendee.findMany({
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          createdAt: true,
-          checkIn: { // checkIn é um objeto que contém as chaves e valores dos dados de check-in
-            select: {
-              createdAt: true,
+      const [attendees, total] = await Promise.all([ // Promisse.all() é um método que retorna uma única promessa que resolve quando todas as promessas no argumento iterável forem resolvidas ou quando o iterável contém zero promessas.])
+        prisma.attendee.findMany({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            createdAt: true,
+            checkIn: { // checkIn é um objeto que contém as chaves e valores dos dados de check-in
+              select: {
+                createdAt: true,
+              }
             }
+          },
+          where: query ? { // Se a query não for nula, procurar por um nome que contenha a query
+            eventId,
+            name: {
+              contains: query, // Procurar por um nome que contenha a query
+            }
+          } : {
+            eventId,
+          },
+          take: 10, // Limitar a 10 participantes por requisição
+          skip: pageIndex * 10, // Pular os participantes das páginas anteriores
+          orderBy: { // Ordenar por data de criação
+            createdAt: 'desc', // Ordenar por data de criação em ordem decrescente
           }
-        },
-        where: query ? { // Se a query não for nula, procurar por um nome que contenha a query
-          eventId,
-          name: {
-            contains: query, // Procurar por um nome que contenha a query
-          }
-        } : {
-          eventId,
-        },
-        take: 10, // Limitar a 10 participantes por requisição
-        skip: pageIndex * 10, // Pular os participantes das páginas anteriores
-        orderBy: { // Ordenar por data de criação
-          createdAt: 'desc', // Ordenar por data de criação em ordem decrescente
-        }
-      })
+        }),
+        prisma.attendee.count({ // Contar o número total de participantes
+          where: query ? { // Se a query não for nula, procurar por um nome que contenha a query
+            eventId,
+            name: {
+              contains: query, // Procurar por um nome que contenha a query
+            }
+          } : {
+            eventId,
+          },
+        })
+      ])
+
       return reply.send({
         attendees: attendees.map(attendee => {
           return {
@@ -70,7 +86,8 @@ export async function getEventAttendees(app: FastifyInstance) {
             createdAt: attendee.createdAt,
             checkedInAt: attendee.checkIn?.createdAt ?? null, // mesma coisa que attendee.checkIn ? attendee.checkIn.createdAt : null
           }
-        })
+        }),
+        total,
       })
     })
 } // Registrar a rota de busca todos participantes de um evento server.ts
